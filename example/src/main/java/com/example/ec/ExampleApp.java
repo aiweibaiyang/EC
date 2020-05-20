817119ceaf2e4046c33c53f30063a42fed46e129
Peihan Zhao <2540070782@qq.com>
2020-05-20 15:37:53 +0800

    第三章 完

---
+++ example/src/main/java/com/example/ec/ExampleApp.java
@@ -0,0 +1,20 @@
+package com.example.ec;
+
+import android.app.Application;
+
+import com.example.latte.app.Latte;
+import com.example.latte.ec.icon.FontEcModule;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
+public class ExampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Latte.init(this)
+                .withApiHost("http://127.0.0.1/")
+                .withIcon(new FontAwesomeModule())
+                .withIcon(new FontEcModule())
+                .configure();
+    }
+}
