47696b778abce5bc8963c20d81188c2cdd45acb0
Peihan Zhao <2540070782@qq.com>
2020-06-09 23:23:59 +0800

    15-3 仿微信自动多图展示及删除控件-2

--- latte-ui/src/main/java/com/example/latte/ui/widget/AutoPhotoLayout.java
+++ latte-ui/src/main/java/com/example/latte/ui/widget/AutoPhotoLayout.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.appcompat.app.AlertDialog;
@@ -37,21 +38,107 @@ public class AutoPhotoLayout extends LinearLayoutCompat {
     private static final List<List<View>> ALL_VIEWS = new ArrayList<>();
     private static final List<Integer> LINE_HEIGHTS = new ArrayList<>();
 
+    //防止多次的测量和布局过程
+    private boolean mIsOnceInitOnMeasure = false;
+    private boolean mHasInitOnLayout = false;
+
     public AutoPhotoLayout(Context context) {
-        super(context,null);
+        super(context, null);
     }
 
     public AutoPhotoLayout(Context context, AttributeSet attrs) {
-        super(context, attrs,0);
+        super(context, attrs, 0);
     }
 
     public AutoPhotoLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.camera_flow_layout);
-        mMaxNum = typedArray.getInt(R.styleable.camera_flow_layout_max_count,1);
-        mMaxLineNum = typedArray.getInt(R.styleable.camera_flow_layout_line_count,3);
-        mImageMargin = typedArray.getInt(R.styleable.camera_flow_layout_item_margin,0);
-        mIconSize = typedArray.getInt(R.styleable.camera_flow_layout_icon_size,20);
+        mMaxNum = typedArray.getInt(R.styleable.camera_flow_layout_max_count, 1);
+        mMaxLineNum = typedArray.getInt(R.styleable.camera_flow_layout_line_count, 3);
+        mImageMargin = typedArray.getInt(R.styleable.camera_flow_layout_item_margin, 0);
+        mIconSize = typedArray.getInt(R.styleable.camera_flow_layout_icon_size, 20);
         typedArray.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int sizeWith = MeasureSpec.getSize(widthMeasureSpec);
+        final int modeWith = MeasureSpec.getMode(widthMeasureSpec);
+        final int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        final int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+        //wrap_content
+        int width = 0;
+        int height = 0;
+        //记录每一行的宽度与高度
+        int lineWith = 0;
+        int lineHeight = 0;
+        //得到内部元素个数
+        int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            final View child = getChildAt(i);
+            //测量子View的宽和高
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            //的搭配LayoutParams
+            final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            //子View占据的宽度
+            final int childWidth = child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            //子View占据的高度
+            final int childHeight = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+            //换行
+            if (lineWith + childWidth > sizeWith - getPaddingLeft() - getPaddingRight()) {
+                //对比得到最大宽度
+                width = Math.max(width, lineWith);
+                //重置lineWidth
+                lineWith = childWidth;
+                height += lineHeight;
+                lineHeight = childHeight;
+            } else {
+                //未换行
+                //叠加行宽
+                lineWith += childWidth;
+                //得到当前最大的高度
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            //最后一个子控件
+            if (i == cCount - 1) {
+                width = Math.max(lineWith, width);
+                //判断是否超过最大拍照限制
+                height += lineHeight;
+            }
+        }
+        setMeasuredDimension(
+                modeWith == MeasureSpec.EXACTLY ? sizeWith : width + getPaddingLeft() + getPaddingRight(),
+                modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height + getPaddingTop() + getPaddingBottom()
+        );
+        //设置一行所有图片的宽高
+        final int imageSideLen = sizeWith / mMaxLineNum;
+        //只初始化一次
+        if (!mIsOnceInitOnMeasure) {
+            mParams = new LayoutParams(imageSideLen, imageSideLen);
+            mIsOnceInitOnMeasure = true;
+        }
+
+    }
+
+    private void initAddIcon(){
+        mIconAdd = new IconTextView(getContext());
+        mIconAdd.setText(ICON_TEXT);
+        mIconAdd.setGravity(Gravity.CENTER);
+        mIconAdd.setTextSize(mIconSize);
+        mIconAdd.setBackgroundResource(R.drawable.border_text);
+        mIconAdd.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDelegate.startCameraWithCheck();
+            }
+        });
+        this.addView(mIconAdd);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        initAddIcon();
+        mTargetDialog = new AlertDialog.Builder(getContext()).create();
     }
 }
